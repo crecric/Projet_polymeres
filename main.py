80e22ef80a76aba0c9ba91f2b31d62f77b1d4ce5
Nikita Allaglo <nikita1999.allaglo@gmail.com>
2024-02-12 04:13:56 +0100

    The random walk now cannot be stuck in a closed-loop of neighbors

--- main.py
+++ main.py
@@ -1,25 +1,29 @@
 import numpy as np
-from calcul import random_step
+from calcul import random_step, number_neighbors
 from visualisation import visu3D
 import matplotlib as mpl
 
 # Grid and length params
-N = 10**3
-n = 100
+N = 10**5
+n = 10000
 
 # Random initial start
 x, y, z = np.random.randint(0, N-1, 3)
 pos = [[x,y,z]]
+
 # Looping on the walk
 for step in range(n):
+   print(step)
+   if number_neighbors(N, [x,y,z], pos) == 6:
+      break
    x, y, z = random_step(N, pos[step])
    while [x, y, z] in pos:
       # Generating new step if the step is already present in the history of steps
       x, y, z = random_step(N, pos[step])
    pos.append([x,y,z])
 
-pos = np.array(pos)
-print(pos)
+pos = np.array(pos).T
+
 # Global plotting parameters
 mpl.rcParams['font.size'] = 15
 mpl.rcParams['figure.figsize'] = (6,5)
