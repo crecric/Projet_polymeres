682dddef0d51880fd2fc0bee497e36d006aaa5ad
Nikita Allaglo <nikita1999.allaglo@gmail.com>
2024-02-12 03:15:59 +0100

    Plotting is nicer and first/last steps are distinguished

--- visualisation.py
+++ visualisation.py
@@ -1,6 +1,12 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def graph2D(x,y):
-    plt.plot(x,y)
+def visu3D(x, y, z):
+    ax = plt.figure().add_subplot(projection='3d')
+    ax.plot(x, y, z, 'ro-', mfc='red', mec='k', ms=6.0, lw=1)
+
+    # Marking the first and last point
+    ax.plot(x[[0,-1]], y[[0,-1]], z[[0,-1]], 'bo', mfc='blue', mec='k', ms=6.0)
+
+    ax.grid(True)
     plt.show()
