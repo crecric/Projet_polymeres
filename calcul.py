da654e117a8a648d1c7c18c937b300868681c1d4
Nikita Allaglo <nikita1999.allaglo@gmail.com>
2024-02-19 13:37:22 +0100

    Self-avoiding base is now POO

--- calcul.py
+++ calcul.py
@@ -1,26 +1,86 @@
 import numpy as np
 from random import choice
 
-def random_step(N, coords):
+class LatticePolymer:
     '''
-    This function generates a random step (with periodic boundary conditions) at position pos in a 3-dimension n-length chain.
-    N**3 is the number of accessible sites.
+    TODO: excluse stuck polymers
     '''
-    x, y, z = coords
-    neighbors = [[(x+1)%N, y, z], [(x-1)%N, y, z], [x, (y+1)%N, z], [x, (y-1)%N, z], [x, y, (z+1)%N], [x, y, (z-1)%N]]
-    x, y, z = choice(neighbors)
+    def __init__(self, N, constraint='force'):
+        self.N = N
+        self.constraint = constraint
+        if self.constraint not in ['force', 'length']:
+            raise NotImplementedError('Please select constraint in ["force", "length"].')
+        
+        # Creating a sufficiently big grid
+        grid = 2*self.N + 1
+        self.pos = [[self.N, self.N, self.N]]
+        self.weight = 1
 
-    return x, y, z
+    def genwalk(self, compute_weight=True):
+        # Looping on the walk
+        for step in range(self.N):
+            if compute_weight:
+                print(self.weight)
+                self.update_weight()
+            # print(self.pos)
+            if self.number_neighbors() == 0:
+                # Stoping the walk when it reaches a closed-loop of neighbors
+                break
+            x, y, z = self.random_step()
+            while [x, y, z] in self.pos:
+                # Generating new step if the step is already present in the history of steps
+                x, y, z = self.random_step()
+            self.pos.append([x,y,z])
 
-def number_neighbors(N, coords, history):
+    def number_neighbors(self):
+        '''
+        This function computes the number of occupied neighbors at a given site in a history of steps. 
+        It is relevent to use in the case where a polymer chain can no longer be extended (the final step is surrounded by 6 neighbors)
+        '''
+        x, y, z = self.pos[-1]
+        neighbors = [[(x+1), y, z], [(x-1), y, z], [x, (y+1), z], [x, (y-1), z], [x, y, (z+1)], [x, y, (z-1)]]
+        c = 0
+        for neighbor in neighbors:
+            if neighbor not in self.pos:
+                c += 1
+        return c
+    
+    def random_step(self):
+        '''
+        This function generates a random step at position pos in a 3-dimension n-length chain.
+        N**3 is the number of accessible sites.
+        '''
+        x, y, z = self.pos[-1]
+        neighbors = [[(x+1), y, z], [(x-1), y, z], [x, (y+1), z], [x, (y-1), z], [x, y, (z+1)], [x, y, (z-1)]]
+        x, y, z = choice(neighbors)
+
+        return x, y, z
+    
+    def update_weight(self):
+        self.weight *= self.number_neighbors()
+
+    # def rosenbluth():
+    
+    # def perm():
+        
+class MonteCarlo(LatticePolymer):
     '''
-    This function computes the number of occupied neighbors at a given site in a history of steps. 
-    It is relevent to use in the case where a polymer chain can no longer be extended (the final step is surrounded by 6 neighbors)
+    Generate collection of polymers
+    Return thermodynamic observables
     '''
-    x, y, z = coords
-    neighbors = [[(x+1)%N, y, z], [(x-1)%N, y, z], [x, (y+1)%N, z], [x, (y-1)%N, z], [x, y, (z+1)%N], [x, y, (z-1)%N]]
-    c = 0
-    for neighbor in neighbors:
-        if neighbor in history:
-            c += 1
-    return c
+    def __init__(self, n=50):
+        self.n = n
+        LatticePolymer.__init__()
+        self.history = np.empty(shape=self.n, dtype=LatticePolymer)
+
+    def rothenbluth(self, perm=False):
+        for trial in range(self.n):
+            self.history.append()
+        return None
+    
+    def compute_re(self, ):
+
+        return None
+    
+# polymer.sample_re(rothenbluth='perm')
+# class Observables(LatticePolymer):
--- main.py
+++ main.py
@@ -1,28 +1,16 @@
 import numpy as np
-from calcul import random_step, number_neighbors
+from calcul import LatticePolymer
 from visualisation import visu3D
 import matplotlib as mpl
 
 # Grid and length params
-N = 10**5
 n = 10000
 
-# Random initial start
-x, y, z = np.random.randint(0, N-1, 3)
-pos = [[x,y,z]]
-
-# Looping on the walk
-for step in range(n):
-   if number_neighbors(N, [x,y,z], pos) == 6:
-      # Stoping the walk when it reaches a closed-loop of neighbors
-      break
-   x, y, z = random_step(N, pos[step])
-   while [x, y, z] in pos:
-      # Generating new step if the step is already present in the history of steps
-      x, y, z = random_step(N, pos[step])
-   pos.append([x,y,z])
-
-pos = np.array(pos).T
+# Generating polymer
+polymer = LatticePolymer(n)
+polymer.genwalk()
+polymer.weight
+pos = np.array(polymer.pos).T
 
 # Global plotting parameters
 mpl.rcParams['font.size'] = 15
